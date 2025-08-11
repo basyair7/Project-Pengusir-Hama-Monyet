30408e9ea9e2c4f19c7bd76b0948d703369aabae
basyair7 <basyair.fathul@gmail.com>
2025-08-11 13:16:04 +0700

    modified run_app.sh

--- run_app.sh
+++ run_app.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /home/pi/Desktop/Project-Pengusir-Hama-Monyet
 source ./venv/bin/activate
-python main.py
+python main.py 2>&1 | while IFS= read -r line; do
+    echo "$(date '+%Y-%m-%d %H:%M:%S') $line"
 done >> /home/pi/Desktop/log.txt
